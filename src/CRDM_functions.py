5b45519931f6f74887248041a6fa5fbf0faf82e7
cdn-pizarro <ricardo.pizarro@nih.gov>
2023-01-11 11:43:09 -0500

    making variable names consitent and descriptive

--- src/CRDM_functions.py
+++ src/CRDM_functions.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 
 
 
-def fit_ambiguity_risk_model(choice_sure_lott_amb, guesses = [1, 0.5, 0.5],
-                             bkbounds = ((0,8),(1e-8,6.4),(1e-8,6.4)),disp=False):
+def fit_ambiguity_risk_model(data_choice_sure_lott_amb, gba_guess = [1, 0.5, 0.5],
+                             gba_bounds = ((0,8),(1e-8,6.4),(1e-8,6.4)),disp=False):
     # We do start the optimizer off with the guesses.
     # The guesses are a starting point in parameter space for the optimizer. 
 
@@ -24,7 +24,7 @@ def fit_ambiguity_risk_model(choice_sure_lott_amb, guesses = [1, 0.5, 0.5],
     # bkbounds = ((0,8),(1e-8,6.4),(1e-8,6.4))
 
     # These are the inputs of the local_negLL function. They'll be passed through optimize_me()
-    inputs = choice_sure_lott_amb.T.values.tolist()
+    inputs = data_choice_sure_lott_amb.T.values.tolist()
     # print(inputs)
 
     # If seeking to improve performance, could change optimization method, could change maxiter(ations), 
@@ -32,74 +32,75 @@ def fit_ambiguity_risk_model(choice_sure_lott_amb, guesses = [1, 0.5, 0.5],
     # You might be able to change the distance between steps in the optimzation.
     # results = optimize.minimize(optimize_me,guesses,inputs, bounds = bkbounds,method='L-BFGS-B', 
     #                             tol=1e-5, callback = None, options={'maxiter':10000, 'disp': True})
-    results = optimize.minimize(optimize_me,guesses,inputs,bounds = bkbounds,
+    results = optimize.minimize(optimize_me,gba_guess,inputs,bounds = gba_bounds,
                                 method='L-BFGS-B',options={'disp':disp})
     negLL = results.fun
-    slope = results.x[0]
+    gamma = results.x[0]
     beta = results.x[1]
     alpha = results.x[2]
     
-    return negLL, slope, beta, alpha
+    return negLL, gamma, beta, alpha
 
 
-def optimize_me(beta, inputs):
-    choices_list,vF,vA,pF,pA,AL = inputs
-    return local_negLL(beta,choices_list,vF,vA,pF,pA,AL)
+def optimize_me(gamma_beta_alpha, inputs):
+    choice,value_fix,value_ambig,p_fix,p_ambig,ambiguity = inputs
+    return function_negLL(gamma_beta_alpha,choice,value_fix,value_ambig,p_fix,p_ambig,ambiguity)
 
 
-def local_negLL(beta,choices_list,vF,vA,pF,pA,AL):
+def function_negLL(gamma_beta_alpha,choice,value_fix,value_ambig,p_fix,p_ambig,ambiguity):
 
-    ps_list,Util_fixed,Util_ambiguous = choice_prob_ambiguity_risk(vF,vA,pF,pA,AL,beta)
-    ps = np.array(ps_list)
-    choices = np.array(choices_list)
+    p_ambig = probability_ambiguity(value_fix,value_ambig,p_fix,p_ambig,ambiguity,gamma_beta_alpha)[0]
+    p_ambig = np.array(p_ambig)
+    choice = np.array(choice)
 
     # Trap log(0). This will prevent the code from trying to calculate the log of 0 in the next section.
-    ps[ps==0] = 1e-6
-    ps[ps==1] = 1-1e-6
+    p_ambig[p_ambig==0] = 1e-6
+    p_ambig[p_ambig==1] = 1-1e-6
     
     # Log-likelihood
-    err = (choices==1)*np.log(ps) + ((choices==0))*np.log(1-ps)
+    LL = (choice==1)*np.log(p_ambig) + ((choice==0))*np.log(1-p_ambig)
 
     # Sum of -log-likelihood
-    sumerr = -sum(err)
+    negLL = -sum(LL)
 
-    return sumerr
+    return negLL
 
 
-def choice_prob_ambiguity_risk(vF,vA,pF,pA,AL,beta):
-    ps = []
-    utility_fixed_list = []
-    utility_ambiguous_list = []
-    ambig_fixed = 0
-    for n in range(len(vF)):
+def probability_ambiguity(value_fix,value_ambig,p_fix,p_ambig,ambiguity,gamma_beta_alpha):
+    p_ambig = []
+    SV_fix = []
+    SV_ambig = []
+    ambig_fix = 0
+    for i,(vf,va,pf,pa,a) in enumerate(zip(value_fix,value_ambig,p_fix,p_ambig,ambiguity)):
         # subjective value (utility) fixed (sure bet)
-        util_fix = ambiguity_utility(vF[n],pF[n],ambig_fixed,beta[2],beta[1])
+        iSV_fix = SV_ambiguity(vf,pf,ambig_fix,gamma_beta_alpha[2],gamma_beta_alpha[1])
         # subjective value (utility) ambiguous (lottery)
-        util_amb = ambiguity_utility(vA[n],pA[n],AL[n],beta[2],beta[1])
+        iSV_ambig = SV_ambiguity(va,pa,a,gamma_beta_alpha[2],gamma_beta_alpha[1])
 
         try: 
-            p = 1 / (1 + math.exp(-beta[0]*(util_amb - util_fix)))
+            p = 1 / (1 + math.exp(-gamma_beta_alpha[0]*(iSV_ambig - iSV_fix)))
             # p = 1 / (1 + math.exp(beta_and_k_array[0]*(SS_SV-LL_SV)))     ## Math.exp does e^(). In other words, if the smaller-sooner SV is higher than the larger-later SV, e^x will be larger, making the denominator larger, making 1/denom closer to zero (low probability of choosing delay). If the LL SV is higher, the e^x will be lower, making 1/denom close to 1 (high probability of choosing delay). If they are the same, e^0=1, 1/(1+1) = 0.5, 50% chance of choosing delay.
         except OverflowError:                                             ## Sometimes the SS_SV is very much higher than the LL_SV. If beta gets too high, the exponent on e will get huge. Math.exp will throw an OverflowError if the numbers get too big. In that case, 1/(1+[something huge]) is essentially zero, so we just set it to 0.
             p = 0
-        ps.append(p)
-        utility_fixed_list.append(util_fix)
-        utility_ambiguous_list.append(util_amb)
+        p_ambig.append(p)
+        SV_fix.append(iSV_fix)
+        SV_ambig.append(iSV_ambig)
         
-    return ps,utility_fixed_list,utility_ambiguous_list
+    return p_ambig,SV_fix,SV_ambig
 
 
-def ambiguity_utility(v,p,AL,alpha,beta):
+def SV_ambiguity(value,p_win,ambiguity,alpha,beta):
+    # subjective value, SV, different when positive and negative
     if v>0:
-        SV = (p - beta*AL/2) * (v**alpha)
+        SV = (p_win - beta*ambiguity/2) * (value**alpha)
     else:
-        SV = (p - beta*AL/2) *(-1.0)*(abs(v)**alpha)
+        SV = (p_win - beta*ambiguity/2) *(-1.0)*(abs(value)**alpha)
     return SV
 
 
 
 
-def analysis(negLL,choices,ps,nb_parms=2):
+def analysis(negLL,choices,p_ambig,nb_parms=2):
     # Unrestricted log-likelihood
     LL = -negLL
 
@@ -110,14 +111,14 @@ def analysis(negLL,choices,ps,nb_parms=2):
     AIC = -2*LL + 2*nb_parms  #CHANGE TO len(results.x) IF USING A DIFFERENT MODEL (parameters != 2)
 
     # Bayesian information criterion
-    BIC = -2*LL + 2*math.log(len(ps))  #len(results.x)
+    BIC = -2*LL + 2*math.log(len(p_ambig))  #len(results.x)
 
     #R squared
     r2 = 1 - LL/LL0
 
     #Percent accuracy
-    parray = np.array(ps) # gets an array of probabilities of choosing the LL choice
-    correct =sum((parray>=0.5)==choices)/len(ps)                                          # LL is 1 in choices, so when the parray is > 0.5 and choices==1, the model has correctly predicted a choice.
+    parray = np.array(p_ambig) # gets an array of probabilities of choosing the LL choice
+    correct =sum((parray>=0.5)==choices)/len(p_ambig)                                          # LL is 1 in choices, so when the parray is > 0.5 and choices==1, the model has correctly predicted a choice.
 
     return LL,LL0,AIC,BIC,r2,correct
 
